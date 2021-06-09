2f1dcd40dbaeccfccf459824f9b2b2eb80700d86
Suraj Kushvaha <71623164+surajkushvaha@users.noreply.github.com>
2021-06-09 22:36:10 +0530

    Delete javaUI directory

--- javaUI/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module javaUI {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.graphics;
-	requires javafx.base;
-	requires java.desktop;
-	
-	opens application to javafx.graphics, javafx.fxml;
-}
